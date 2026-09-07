d48a55a08ee24972ce9db7e047996318a7e64d50
Pete <pete.bankhead@gmail.com>
2026-09-07 14:32:50 +0200

    Initial OmeZarrImageServerBuilder

---
+++ src/main/java/qupath/ext/zarr/OmeZarrImageServerBuilder.java
@@ -0,0 +1,116 @@
+package qupath.ext.zarr;
+
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.view.Views;
+import ome.zarr.imglib2.PyramidBackend;
+import ome.zarr.imglib2.PyramidContents;
+import ome.zarr.imglib2.ZarrUtils;
+import ome.zarr.imglib2.metadata.AxisCalibration;
+import ome.zarr.imglib2.metadata.Omero;
+import ome.zarr.zarrjava.ZarrJavaPyramidBackend;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import qupath.ext.imglib2.ImgBuilder;
+import qupath.ext.imglib2.ImgLib2ImageServer;
+import qupath.lib.images.servers.ImageChannel;
+import qupath.lib.images.servers.ImageServer;
+import qupath.lib.images.servers.ImageServerBuilder;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class OmeZarrImageServerBuilder implements ImageServerBuilder<BufferedImage> {
+
+    private static final Logger logger = LoggerFactory.getLogger(OmeZarrImageServerBuilder.class);
+
+    @Override
+    public UriImageSupport<BufferedImage> checkImageSupport(URI uri, String... args) throws IOException {
+        float supportLevel = 0f;
+        if (ZarrUtils.isZarr(uri))
+            supportLevel = 5f;
+        return UriImageSupport.createInstance(
+                OmeZarrImageServerBuilder.class,
+                supportLevel,
+                DefaultImageServerBuilder.createInstance(
+                        OmeZarrImageServerBuilder.class, uri, args
+                ));
+    }
+
+    @Override
+    public ImageServer<BufferedImage> buildServer(URI uri, String... args) throws Exception {
+        return create(uri, args);
+    }
+
+    private < T extends NativeType< T > & RealType< T >> ImageServer<BufferedImage> create(URI uri, String... args) throws Exception {
+        PyramidBackend backend = new ZarrJavaPyramidBackend();
+        PyramidContents<T> contents = backend.read(uri);
+        List<RandomAccessibleInterval<T>> resolutions = new ArrayList<>();
+        AxisCalibration[] axes = contents.axesPerLevel[0];
+        for (int r = 0; r < contents.numResolutionLevels(); r++) {
+            RandomAccessibleInterval<T> img = contents.asImg(r);
+            if (img.numDimensions() != 5) {
+                throw new IOException("Invalid image dimensions: " + img.numDimensions());
+            }
+            // Switch XYCZT to XYZCT
+            img = Views.moveAxis(img, 2, 3);
+            resolutions.add(img);
+        }
+        var builder = ImgLib2ImageServer.builder(resolutions);
+        var channels = parseChannels(contents.omero);
+        long expectedChannels = resolutions.getFirst().dimension(ImgBuilder.AXIS_CHANNEL);
+        if (channels.size() == expectedChannels) {
+            builder = builder.channels(channels);
+        } else if (!channels.isEmpty()) {
+            logger.warn("Expected {} channels, found {}", expectedChannels, channels.size());
+        }
+        return builder.build();
+    }
+
+    private static List<ImageChannel> parseChannels(Omero omero) {
+        List<ImageChannel> channels = new ArrayList<>();
+        for (int c = 0; c < omero.channels.size(); c++) {
+            var channel = omero.channels.get(c);
+            String name = channel.label;
+            Integer color = parseColor(channel.color).orElse(ImageChannel.getDefaultChannelColor(c));
+            channels.add(ImageChannel.getInstance(name, color));
+        }
+        return channels;
+    }
+
+    private static Optional<Integer> parseColor(String color) {
+        if (color != null && !color.isBlank()) {
+            try {
+                return Optional.of(
+                        Color.decode(color).getRGB()
+                );
+            } catch (Exception e) {
+                logger.warn("Invalid image color: {}", color);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public String getName() {
+        return "OME-Zarr server builder";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Image server builder to support OME-Zarr";
+    }
+
+    @Override
+    public Class<BufferedImage> getImageType() {
+        return BufferedImage.class;
+    }
+
+}
