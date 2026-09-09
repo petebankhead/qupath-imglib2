19048538575c4a51f4c248b75d4df6a679244842
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2026-09-09 13:47:08 +0200

    Very marginally less catastrophically stupid implementation

--- src/main/java/qupath/ext/imglib2/CachedPixelClassifierServer.java
+++ src/main/java/qupath/ext/imglib2/CachedPixelClassifierServer.java
@@ -3,6 +3,7 @@ package qupath.ext.imglib2;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import qupath.ext.zarr.OmeZarrImageServerBuilder;
 import qupath.lib.classifiers.pixel.PixelClassificationImageServer;
@@ -10,24 +11,46 @@ import qupath.lib.classifiers.pixel.PixelClassifier;
 import qupath.lib.images.ImageData;
 import qupath.lib.images.servers.TileRequest;
 import qupath.lib.images.writers.ome.zarr.PyramidalOMEZarrWriter;
+import qupath.lib.projects.ProjectImageEntry;
+import qupath.lib.scripting.QP;
+
+
+import static ome.zarr.imglib2.ZarrUtils.isZarr;
 
 public class CachedPixelClassifierServer extends PixelClassificationImageServer {
 
-    private final String cachePath;
     private final ImgLib2ImageServer<?> omeServer;
 
-    public CachedPixelClassifierServer(ImageData<BufferedImage> imageData, PixelClassifier classifier) {
-        super(imageData, classifier);
+    public CachedPixelClassifierServer(ProjectImageEntry<BufferedImage> projectImageEntry, String pixelClassifierName) throws IOException {
+        this(projectImageEntry.getEntryPath().resolve("cache").resolve(pixelClassifierName + ".ome.zarr"),
+                projectImageEntry.readImageData(),
+                QP.getProject().getPixelClassifiers().get(pixelClassifierName));
+    }
+
+
+    public CachedPixelClassifierServer(ProjectImageEntry<BufferedImage> projectImageEntry, PixelClassifier pixelClassifier) throws IOException {
+        this(projectImageEntry.getEntryPath().resolve("cache").resolve(pixelClassifier.hashCode() + ".ome.zarr"),
+                projectImageEntry.readImageData(),
+                pixelClassifier);
+    }
+
+    public CachedPixelClassifierServer(Path cachePath, ImageData<BufferedImage> imageData, PixelClassifier pixelClassifier) {
+        super(imageData, pixelClassifier);
         try {
-            cachePath = "/tmp/cached.ome.zarr";
-            var pozw = new PyramidalOMEZarrWriter.Builder(this)
-                    .downsamples(2, 4, 8)
-                    .build(cachePath);
-            pozw.writeImage();
-            this.omeServer = (ImgLib2ImageServer<?>) new OmeZarrImageServerBuilder().buildServer(Path.of(cachePath).toUri());
+            if (!isZarr(cachePath.toUri())) {
+                var pozw = new PyramidalOMEZarrWriter.Builder(this)
+                        .downsamples(2, 4, 8)
+                        .build(cachePath.toString());
+                pozw.writeImage();
+            }
+            this.omeServer = (ImgLib2ImageServer<?>) new OmeZarrImageServerBuilder().buildServer(cachePath.toUri());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public CachedPixelClassifierServer(ImageData<BufferedImage> imageData, PixelClassifier classifier) throws IOException {
+        this(Files.createTempFile("qupath_", ".ome.zarr"), imageData, classifier);
     }
 
     @Override
